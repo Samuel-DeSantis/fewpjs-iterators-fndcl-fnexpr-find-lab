1f4150b0e19522f075e45ae647369d93cda5c3c6
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-08-28 17:12:31 -0600

    commit tree bbbb8814994964411e5bb45152efa41e452e727d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin (record) {
+    if (record.find ( game => game.result === 'W')) {
+        return record.find ( game => game.result === 'W').year
+    } else {
+        return undefined
+    }
+}
